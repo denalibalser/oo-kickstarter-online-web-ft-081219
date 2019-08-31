9e37c2856242ffe2497a5edb414d2dd000917c7c
Denali Balser <denali.balser167@gmail.com>
2019-08-31 15:14:16 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    backer.add_project(self) unless @backed_projects.include?(self)
+    backer.add_project(self) unless project.backers.include?(self)
   end
 end
